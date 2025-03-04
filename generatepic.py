d45779b224d1e5fde5baefeaf7a01271f0c9dbf3
Alex <mathewalex806@gmail.com>
2025-03-04 19:31:14 +0530

    changes

--- generatepic.py
+++ generatepic.py
@@ -1,13 +1,14 @@
 from images import Image
-image = Image("Untitled.gif")
+image = Image("pic.gif")
 image.draw()
-black_pixel = (0, 0, 0)
-white_pixel = (255, 255, 255)
-for y in range (image.getHeight()):
-    for x in range (image.getWidth()):
-        (r,g,b) = image.getPixel(x, y)
-        avg = (r+g+b)//3
-        if avg < 128:
-            image.setPixel(x, y, black_pixel)
-        else:
-            image.setPixel(x, y, white_pixel)
+# black_pixel = (0, 0, 0)
+# white_pixel = (255, 255, 255)
+# for y in range (image.getHeight()):
+#     for x in range (image.getWidth()):
+#         (r,g,b) = image.getPixel(x, y)
+#         avg = (r+g+b)//3
+#         if avg < 128:
+#             image.setPixel(x, y, black_pixel)
+#         else:
+#             image.setPixel(x, y, white_pixel)
+# image.draw()
